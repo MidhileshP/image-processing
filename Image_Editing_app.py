018fc0e7acbed074625ddf28d40e652dcf9cacbe
midhileshp <124782075+MidhileshP@users.noreply.github.com>
2023-03-29 22:12:18 +0530

    Update Image_Editing_app.py

--- Image_Editing_app.py
+++ Image_Editing_app.py
@@ -27,75 +27,73 @@ def cartoonize_image(our_image):
     canny=cv2.Canny(img,100,150)
     return canny
 
-    
-st.title('Image Editing App')
-st.text("Edit your images in a fast and simple way")
-activities=["Detection","About"]
-choice=st.sidebar.selectbox("Select Activity",activities)
-if choice=="Detection":
-    st.subheader("Detection")
-    image_file=st.file_uploader("Upload Image",type=["jpg","jpeg","png"])
-    if image_file is not None:
-        our_image=Image.open(image_file)
-        st.text("Original Image")
-        st.image(our_image)
-        
-        enhance_type=st.sidebar.radio("Enhance type", ["Original-Compressed","Gray-Scale","Contrast","Brightness","Blurring","Sharpness"])
-        if enhance_type=="Original-Compressed":
-            st.image(our_image,width=300)
-        elif enhance_type=="Gray-Scale":
-            img=np.array(our_image.convert("RGB"))
-            gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            st.text("Gray-Scaled Image")
-            st.image(gray)
-        elif enhance_type=="Contrast":
-            rate=st.sidebar.slider("Contrast",0.1,6.0)#Use only float numbers
-            enhancer=ImageEnhance.Contrast(our_image)
-            enhanced_img=enhancer.enhance(rate)
-            st.text("Contrasted Image")
-            st.image(enhanced_img)
-        elif enhance_type=="Brightness":
-            rate=st.sidebar.slider("Brightness",0.1,10.0)
-            enhancer=ImageEnhance.Brightness(our_image)
-            brighted_img=enhancer.enhance(rate)
-            st.text("Brighted Image")
-            st.image(brighted_img)
-        elif enhance_type=="Blurring":
-            rate=st.sidebar.slider("Brightness",0.1,10.0)
-            blurred_img=cv2.GaussianBlur(np.array(our_image),(17,15),rate)#Use only odd numbers
-            st.text("Blurred Image")
-            st.image(blurred_img)
-        elif enhance_type=="Sharpness":
-            rate=st.sidebar.slider("Sharpness",0.1,10.0)
-            enhancer=ImageEnhance.Sharpness(our_image)
-            sharpened_img=enhancer.enhance(rate)
-            st.text("Sharpened Image")
-            st.image(sharpened_img)
-        else:
+def main_loop():    
+    st.title('Image Editing App')
+    st.text("Edit your images in a fast and simple way")
+    activities=["Detection","About"]
+    choice=st.sidebar.selectbox("Select Activity",activities)
+    if choice=="Detection":
+        st.subheader("Detection")
+        image_file=st.file_uploader("Upload Image",type=["jpg","jpeg","png"])
+        if image_file is not None:
+            our_image=Image.open(image_file)
+            st.text("Original Image")
             st.image(our_image)
         
-    tasks=["Faces","Eyes","Cartoonize"]
-    feature_choice=st.sidebar.selectbox("Find features",tasks)
-    if st.button("Process"):
-        if feature_choice=="Faces":
-            result_img,result_faces = detect_faces(our_image)
-            st.image(result_img)
-            st.success("Found {} faces".format(len(result_faces)))
-        elif feature_choice=="Eyes":
-            result_img = detect_eyes(our_image)
-            st.image(result_img)
-        elif feature_choice=="Cartoonize":
-            result_img = cartoonize_image(our_image)
-            st.image(result_img)
+            enhance_type=st.sidebar.radio("Enhance type", ["Original-Compressed","Gray-Scale","Contrast","Brightness","Blurring","Sharpness"])
+            if enhance_type=="Original-Compressed":
+                st.image(our_image,width=300)
+            elif enhance_type=="Gray-Scale":
+                img=np.array(our_image.convert("RGB"))
+                gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+                st.text("Gray-Scaled Image")
+                st.image(gray)
+            elif enhance_type=="Contrast":
+                rate=st.sidebar.slider("Contrast",0.1,6.0)#Use only float numbers
+                enhancer=ImageEnhance.Contrast(our_image)
+                enhanced_img=enhancer.enhance(rate)
+                st.text("Contrasted Image")
+                st.image(enhanced_img)
+            elif enhance_type=="Brightness":
+                rate=st.sidebar.slider("Brightness",0.1,10.0)
+                enhancer=ImageEnhance.Brightness(our_image)
+                brighted_img=enhancer.enhance(rate)
+                st.text("Brighted Image")
+                st.image(brighted_img)
+            elif enhance_type=="Blurring":
+                rate=st.sidebar.slider("Brightness",0.1,10.0)
+                blurred_img=cv2.GaussianBlur(np.array(our_image),(17,15),rate)#Use only odd numbers
+                st.text("Blurred Image")
+                st.image(blurred_img)
+            elif enhance_type=="Sharpness":
+                rate=st.sidebar.slider("Sharpness",0.1,10.0)
+                enhancer=ImageEnhance.Sharpness(our_image)
+                sharpened_img=enhancer.enhance(rate)
+                st.text("Sharpened Image")
+                st.image(sharpened_img)
+            else:
+                st.image(our_image)
+            
+        tasks=["Faces","Eyes","Cartoonize"]
+        feature_choice=st.sidebar.selectbox("Find features",tasks)
+        if st.button("Process"):
+            if feature_choice=="Faces":
+                result_img,result_faces = detect_faces(our_image)
+                st.image(result_img)
+                st.success("Found {} faces".format(len(result_faces)))
+            elif feature_choice=="Eyes":
+                result_img = detect_eyes(our_image)
+                st.image(result_img)
+            elif feature_choice=="Cartoonize":
+                result_img = cartoonize_image(our_image)
+                st.image(result_img)
+            
         
         
-        
-        
-        
-elif choice=="About":
-    st.subheader("About the developer")
-    st.markdown("Built with streamlit by [Team1]")
-    st.text("Our team built this as a Mini-Project for Cloud Computing")
-    clou=Image.open("Aboutfor.jpg")
-    st.image(clou)
-    st.text("We have a basic understanding on Python and C languages")
+    elif choice=="About":
+        st.subheader("About the developer")
+        st.markdown("Built with streamlit by [Team1]")
+        st.text("Our team built this as a Mini-Project for Cloud Computing")
+        clou=Image.open("Aboutfor.jpg")
+        st.image(clou)
+        st.text("We have a basic understanding on Python and C languages")
