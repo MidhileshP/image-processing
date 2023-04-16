27cf561c117933cbfe63455ac4cde6e63359a86e
midhileshp <124782075+MidhileshP@users.noreply.github.com>
2023-04-16 23:07:17 +0530

    Update Image_Editing_app.py

--- Image_Editing_app.py
+++ Image_Editing_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit import streamlit_authenticator as stauth
+import streamlit_authenticator as stauth
 hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
 import yaml
 from yaml.loader import SafeLoader
