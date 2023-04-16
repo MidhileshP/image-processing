556bae5fc509410a4efcc23719703ddf6ecb57c9
midhileshp <124782075+MidhileshP@users.noreply.github.com>
2023-04-16 23:27:12 +0530

    Update Image_Editing_app.py

--- Image_Editing_app.py
+++ Image_Editing_app.py
@@ -4,19 +4,6 @@ from PIL import Image, ImageEnhance
 import numpy as np
 import os
 
-def detect_faces(our_image):
-    new_img=np.array(our_image.convert("RGB"))
-    faces=face_cascade.detectMultiScale(new_img,1.1,6)
-    for(x,y,w,h) in faces:
-        cv2.rectangle(new_img,(x,y),(x+w,y+h),(255,0,0),2)
-    return new_img,faces 
-
-def detect_eyes(our_image):
-    new_img=np.array(our_image.convert("RGB"))
-    eyes=eye_cascade.detectMultiScale(new_img,1.3,6)
-    for(x,y,w,h) in eyes:
-        cv2.rectangle(new_img,(x,y),(x+w,y+h),(0,255,0),2)
-    return new_img
 
 def cartoonize_image(our_image):
     new_img=np.array(our_image.convert("RGB"))
@@ -70,17 +57,10 @@ if choice=="Detection":
             st.image(sharpened_img)
         else:
             st.image(our_image)
-    tasks=["Faces","Eyes","Cartoonize"]
+    tasks=["Cartoonize"]
     feature_choice=st.sidebar.selectbox("Find features",tasks)
     if st.button("Process"):
-        if feature_choice=="Faces":
-            result_img,result_faces = detect_faces(our_image)
-            st.image(result_img)
-            st.success("Found {} faces".format(len(result_faces)))
-        elif feature_choice=="Eyes":
-            result_img = detect_eyes(our_image)
-            st.image(result_img)
-        elif feature_choice=="Cartoonize":
+        if feature_choice=="Cartoonize":
             result_img = cartoonize_image(our_image)
             st.image(result_img)
         
