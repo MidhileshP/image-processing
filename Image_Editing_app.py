1f639e92f38d2a200059bc22685a527606806723
midhileshp <124782075+MidhileshP@users.noreply.github.com>
2023-04-16 21:37:29 +0530

    Update Image_Editing_app.py

--- Image_Editing_app.py
+++ Image_Editing_app.py
@@ -1,99 +1,172 @@
+import pyrebase
 import streamlit as st
-import pandas as pd
-
-
-# Security
-#passlib,hashlib,bcrypt,scrypt
-import hashlib
-def make_hashes(password):
-	return hashlib.sha256(str.encode(password)).hexdigest()
-
-def check_hashes(password,hashed_text):
-	if make_hashes(password) == hashed_text:
-		return hashed_text
-	return False
-# DB Management
-import sqlite3 
-conn = sqlite3.connect('data.db')
-c = conn.cursor()
-# DB  Functions
-def create_usertable():
-	c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT,password TEXT)')
-
-
-def add_userdata(username,password):
-	c.execute('INSERT INTO userstable(username,password) VALUES (?,?)',(username,password))
-	conn.commit()
-
-def login_user(username,password):
-	c.execute('SELECT * FROM userstable WHERE username =? AND password = ?',(username,password))
-	data = c.fetchall()
-	return data
-
-
-def view_all_users():
-	c.execute('SELECT * FROM userstable')
-	data = c.fetchall()
-	return data
+from datetime import datetime
 
 
 
-def main():
-	"""Simple Login App"""
-
-	st.title("Simple Login App")
-
-	menu = ["Home","Login","SignUp"]
-	choice = st.sidebar.selectbox("Menu",menu)
-
-	if choice == "Home":
-		st.subheader("Home")
-
-	elif choice == "Login":
-		st.subheader("Login Section")
-
-		username = st.sidebar.text_input("User Name")
-		password = st.sidebar.text_input("Password",type='password')
-		if st.sidebar.checkbox("Login"):
-			# if password == '12345':
-			create_usertable()
-			hashed_pswd = make_hashes(password)
-
-			result = login_user(username,check_hashes(password,hashed_pswd))
-			if result:
-
-				st.success("Logged In as {}".format(username))
-
-				task = st.selectbox("Task",["Add Post","Analytics","Profiles"])
-				if task == "Add Post":
-					st.subheader("Add Your Post")
-
-				elif task == "Analytics":
-					st.subheader("Analytics")
-				elif task == "Profiles":
-					st.subheader("User Profiles")
-					user_result = view_all_users()
-					clean_db = pd.DataFrame(user_result,columns=["Username","Password"])
-					st.dataframe(clean_db)
-			else:
-				st.warning("Incorrect Username/Password")
+firebaseConfig = {
+  'apiKey': "AIzaSyD2q4XFjnO8Az3pPsZYu7Q7yCUvfW48nTQ",
+  'authDomain': "test-firestore-stream-80056.firebaseapp.com",
+  'projectId': "test-firestore-stream-80056",
+  'databaseURL':"https://console.firebase.google.com/u/0/project/test-firestore-stream-80056/database/test-firestore-stream-80056-default-rtdb/data/~2F",
+  'storageBucket': "test-firestore-stream-80056.appspot.com",
+  'messagingSenderId': "118430717598",
+  'appId': "1:118430717598:web:a27bdaceefbbfc967c8d8f",
+  'measurementId': "G-W3QHVG3L42"
+}
 
 
+# Firebase Authentication
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
 
+# Database
+db = firebase.database()
+storage = firebase.storage()
+st.sidebar.title("Our community app")
 
+# Authentication
+choice = st.sidebar.selectbox('login/Signup', ['Login', 'Sign up'])
 
-	elif choice == "SignUp":
-		st.subheader("Create New Account")
-		new_user = st.text_input("Username")
-		new_password = st.text_input("Password",type='password')
+# Obtain User Input for email and password
+email = st.sidebar.text_input('Please enter your email address')
+password = st.sidebar.text_input('Please enter your password',type = 'password')
 
-		if st.button("Signup"):
-			create_usertable()
-			add_userdata(new_user,make_hashes(new_password))
-			st.success("You have successfully created a valid Account")
-			st.info("Go to Login Menu to login")
+# App 
 
+# Sign up Block
+if choice == 'Sign up':
+    handle = st.sidebar.text_input(
+        'Please input your app handle name', value='Default')
+    submit = st.sidebar.button('Create my account')
 
+    if submit:
+        user = auth.create_user_with_email_and_password(email, password)
+        st.success('Your account is created suceesfully!')
+        st.balloons()
+        # Sign in
+        user = auth.sign_in_with_email_and_password(email, password)
+        db.child(user['localId']).child("Handle").set(handle)
+        db.child(user['localId']).child("ID").set(user['localId'])
+        st.title('Welcome' + handle)
+        st.info('Login via login drop down selection')
 
-if __name__ == '__main__':
-	main()
+# Login Block
+if choice == 'Login':
+    login = st.sidebar.checkbox('Login')
+    if login:
+        user = auth.sign_in_with_email_and_password(email,password)
+        st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+        bio = st.radio('Jump to',['Home','Workplace Feeds', 'Settings'])
+        
+# SETTINGS PAGE 
+        if bio == 'Settings':  
+            # CHECK FOR IMAGE
+            nImage = db.child(user['localId']).child("Image").get().val()    
+            # IMAGE FOUND     
+            if nImage is not None:
+                # We plan to store all our image under the child image
+                Image = db.child(user['localId']).child("Image").get()
+                for img in Image.each():
+                    img_choice = img.val()
+                    #st.write(img_choice)
+                st.image(img_choice)
+                exp = st.beta_expander('Change Bio and Image')  
+                # User plan to change profile picture  
+                with exp:
+                    newImgPath = st.text_input('Enter full path of your profile imgae')
+                    upload_new = st.button('Upload')
+                    if upload_new:
+                        uid = user['localId']
+                        fireb_upload = storage.child(uid).put(newImgPath,user['idToken'])
+                        a_imgdata_url = storage.child(uid).get_url(fireb_upload['downloadTokens']) 
+                        db.child(user['localId']).child("Image").push(a_imgdata_url)
+                        st.success('Success!')           
+            # IF THERE IS NO IMAGE
+            else:    
+                st.info("No profile picture yet")
+                newImgPath = st.text_input('Enter full path of your profile image')
+                upload_new = st.button('Upload')
+                if upload_new:
+                    uid = user['localId']
+                    # Stored Initated Bucket in Firebase
+                    fireb_upload = storage.child(uid).put(newImgPath,user['idToken'])
+                    # Get the url for easy access
+                    a_imgdata_url = storage.child(uid).get_url(fireb_upload['downloadTokens']) 
+                    # Put it in our real time database
+                    db.child(user['localId']).child("Image").push(a_imgdata_url)
+ # HOME PAGE
+        elif bio == 'Home':
+            col1, col2 = st.beta_columns(2)
+            
+            # col for Profile picture
+            with col1:
+                nImage = db.child(user['localId']).child("Image").get().val()         
+                if nImage is not None:
+                    val = db.child(user['localId']).child("Image").get()
+                    for img in val.each():
+                        img_choice = img.val()
+                    st.image(img_choice,use_column_width=True)
+                else:
+                    st.info("No profile picture yet. Go to Edit Profile and choose one!")
+                
+                post = st.text_input("Let's share my current mood as a post!",max_chars = 100)
+                add_post = st.button('Share Posts')
+            if add_post:   
+                now = datetime.now()
+                dt_string = now.strftime("%d/%m/%Y %H:%M:%S")              
+                post = {'Post:' : post,
+                        'Timestamp' : dt_string}                           
+                results = db.child(user['localId']).child("Posts").push(post)
+                st.balloons()
+
+            # This coloumn for the post Display
+            with col2:
+                
+                all_posts = db.child(user['localId']).child("Posts").get()
+                if all_posts.val() is not None:    
+                    for Posts in reversed(all_posts.each()):
+                            #st.write(Posts.key()) # Morty
+                            st.code(Posts.val(),language = '') 
+   # WORKPLACE FEED PAGE
+        else:
+            all_users = db.get()
+            res = []
+            # Store all the users handle name
+            for users_handle in all_users.each():
+                k = users_handle.val()["Handle"]
+                res.append(k)
+            # Total users
+            nl = len(res)
+            st.write('Total users here: '+ str(nl)) 
+            
+            # Allow the user to choose which other user he/she wants to see 
+            choice = st.selectbox('My Collegues',res)
+            push = st.button('Show Profile')
+            
+            # Show the choosen Profile
+            if push:
+                for users_handle in all_users.each():
+                    k = users_handle.val()["Handle"]
+                    # 
+                    if k == choice:
+                        lid = users_handle.val()["ID"]
+                        
+                        handlename = db.child(lid).child("Handle").get().val()             
+                        
+                        st.markdown(handlename, unsafe_allow_html=True)
+                        
+                        nImage = db.child(lid).child("Image").get().val()         
+                        if nImage is not None:
+                            val = db.child(lid).child("Image").get()
+                            for img in val.each():
+                                img_choice = img.val()
+                                st.image(img_choice)
+                        else:
+                            st.info("No profile picture yet. Go to Edit Profile and choose one!")
+ 
+                        # All posts
+                        all_posts = db.child(lid).child("Posts").get()
+                        if all_posts.val() is not None:    
+                            for Posts in reversed(all_posts.each()):
+                                st.code(Posts.val(),language = '')
