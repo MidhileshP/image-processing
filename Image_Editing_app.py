7e1de68110fb369dc5ba5f2035b4f9938241e76d
midhileshp <124782075+MidhileshP@users.noreply.github.com>
2023-04-16 23:01:29 +0530

    Update Image_Editing_app.py

--- Image_Editing_app.py
+++ Image_Editing_app.py
@@ -1,51 +1,60 @@
 import streamlit as st
-import random
+import streamlit_authenticator as stauth
+hashed_passwords = stauth.Hasher(['abc', 'def']).generate()
+import yaml
+from yaml.loader import SafeLoader
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-# define a dictionary with user details
-users = {
-    "user1": {"password": "pass123", "otp": None},
-    "user2": {"password": "pass456", "otp": None},
-    "user3": {"password": "pass789", "otp": None}
-}
-
-# define a function to generate OTP
-def generate_otp():
-    otp = ""
-    for i in range(4):
-        otp += str(random.randint(1, 9))
-    return otp
-
-# define the Streamlit app
-def app():
-    st.title("Login Page")
-
-    # define a form for user input
-    with st.form(key='login_form'):
-        username = st.text_input("Username")
-        password = st.text_input("Password", type='password')
-        submitted = st.form_submit_button('Login')
-
-        # check if the form is submitted
-        if submitted:
-            # check if the username exists
-            if username in users.keys():
-                # check if the password is correct
-                if password == users[username]["password"]:
-                    # generate and send OTP
-                    otp = generate_otp()
-                    users[username]["otp"] = otp
-                    st.success("Login successful. OTP sent to your registered mobile number.")
-                    # display OTP verification form
-                    with st.form(key='otp_form'):
-                        otp_input = st.text_input("Enter OTP")
-                        otp_submitted = st.form_submit_button('Verify OTP')
-                        # check if the OTP is verified
-                        if otp_submitted:
-                            if otp_input == users[username]["otp"]:
-                                st.success("OTP verification successful. You are logged in.")
-                            else:
-                                st.error("Invalid OTP. Please try again.")
-                else:
-                    st.error("Incorrect password. Please try again.")
-            else:
-                st.error("Username not found. Please try again.")
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+name, authentication_status, username = authenticator.login('Login', 'main')
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status is False:
+    st.error('Username/password is incorrect')
+elif authentication_status is None:
+    st.warning('Please enter your username and password')
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
+if authentication_status:
+    try:
+        if authenticator.reset_password(username, 'Reset password'):
+            st.success('Password modified successfully')
+    except Exception as e:
+        st.error(e)
+try:
+    if authenticator.register_user('Register user', preauthorization=False):
+        st.success('User registered successfully')
+except Exception as e:
+    st.error(e)
+try:
+    username_forgot_pw, email_forgot_password, random_password = authenticator.forgot_password('Forgot password')
+    if username_forgot_pw:
+        st.success('New password sent securely')
+        # Random password to be transferred to user securely
+    else:
+        st.error('Username not found')
+except Exception as e:
+    st.error(e)
+if authentication_status:
+    try:
+        if authenticator.update_user_details(username, 'Update user details'):
+            st.success('Entries updated successfully')
+    except Exception as e:
+        st.error(e)
+with open('../config.yaml', 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)
