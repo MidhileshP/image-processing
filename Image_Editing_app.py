6872c03b6455fd88442d85047c3747c1584870aa
midhileshp <124782075+MidhileshP@users.noreply.github.com>
2023-04-23 22:30:23 +0530

    Update Image_Editing_app.py

--- Image_Editing_app.py
+++ Image_Editing_app.py
@@ -3,73 +3,60 @@ import cv2
 from PIL import Image, ImageEnhance
 import numpy as np
 import os
+
+
+def apply_enhancements(our_image, enhance_type):
+    img = np.array(our_image.convert("RGB"))
+    if "Gray-Scale" in enhance_type:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if "Contrast" in enhance_type:
+        rate = st.sidebar.slider("Contrast", 0.1, 6.0)  # Use only float numbers
+        enhancer = ImageEnhance.Contrast(Image.fromarray(img))
+        img = np.array(enhancer.enhance(rate))
+    if "Brightness" in enhance_type:
+        rate = st.sidebar.slider("Brightness", 0.1, 10.0)
+        enhancer = ImageEnhance.Brightness(Image.fromarray(img))
+        img = np.array(enhancer.enhance(rate))
+    if "Blurring" in enhance_type:
+        rate = st.sidebar.slider("Blur", 0.1, 10.0)
+        img = cv2.GaussianBlur(img, (17, 15), rate)  # Use only odd numbers
+    if "Sharpness" in enhance_type:
+        rate = st.sidebar.slider("Sharpness", 0.1, 10.0)
+        enhancer = ImageEnhance.Sharpness(Image.fromarray(img))
+        img = np.array(enhancer.enhance(rate))
+    return img
+
+
 def cartoonize_image(our_image):
-    new_img=np.array(our_image.convert("RGB"))
-    img=cv2.GaussianBlur(new_img,(13,13),0)#0 is Standard Deviation and Kernel should be odd
-    canny=cv2.Canny(img,100,150)
+    new_img = np.array(our_image.convert("RGB"))
+    img = cv2.GaussianBlur(new_img, (13, 13), 0)  # 0 is Standard Deviation and Kernel should be odd
+    canny = cv2.Canny(img, 100, 150)
     return canny
 
-    
+
 st.title('Image Editing App')
 st.text("Edit your images in a fast and simple way")
-activities=["Detection","About"]
-choice=st.sidebar.selectbox("Select Activity",activities)
-if choice=="Detection":
+activities = ["Detection", "About"]
+choice = st.sidebar.selectbox("Select Activity", activities)
+
+if choice == "Detection":
     st.subheader("Detection")
-    image_file=st.file_uploader("Upload Image",type=["jpg","jpeg","png"])
+    image_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+
     if image_file is not None:
-        our_image=Image.open(image_file)
+        our_image = Image.open(image_file)
         st.text("Original Image")
         st.image(our_image)
-        
-        enhance_type=st.sidebar.multiselect("Enhance type",["Original-Compressed","Gray-Scale","Contrast","Brightness","Blurring","Sharpness"])
-        if "Original-Compressed" in enhance_type:
-            st.image(our_image, width=300)
-        if "Gray-Scale" in enhance_type:
-            img = np.array(our_image.convert("RGB"))
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            st.text("Gray-Scaled Image")
-            st.image(gray)
-        if "Contrast" in enhance_type:
-            rate = st.sidebar.slider("Contrast", 0.1, 6.0)  # Use only float numbers
-            enhancer = ImageEnhance.Contrast(our_image)
-            enhanced_img = enhancer.enhance(rate)
-            st.text("Contrasted Image")
-            st.image(enhanced_img)
-        if "Brightness" in enhance_type:
-            rate = st.sidebar.slider("Brightness", 0.1, 10.0)
-            enhancer = ImageEnhance.Brightness(our_image)
-            brighted_img = enhancer.enhance(rate)
-            st.text("Brighted Image")
-            st.image(brighted_img)
-        if "Blurring" in enhance_type:
-            rate = st.sidebar.slider("Blur", 0.1, 10.0)
-            blurred_img = cv2.GaussianBlur(np.array(our_image), (17, 15), rate)  # Use only odd numbers
-            st.text("Blurred Image")
-            st.image(blurred_img)
-        if "Sharpness" in enhance_type:
-            rate = st.sidebar.slider("Sharpness", 0.1, 10.0)
-            enhancer = ImageEnhance.Sharpness(our_image)
-            sharpened_img = enhancer.enhance(rate)
-            st.text("Sharpened Image")
-            st.image(sharpened_img)
-        else:
-            st.image(our_image)
-    tasks=["Cartoonize"]
-    feature_choice=st.sidebar.selectbox("Find features",tasks)
+
+        enhance_type = st.sidebar.multiselect("Enhance type",
+                                              ["Original-Compressed", "Gray-Scale", "Contrast", "Brightness", "Blurring", "Sharpness"])
+        enhanced_img = apply_enhancements(our_image, enhance_type)
+        st.text("Enhanced Image")
+        st.image(enhanced_img)
+
+    tasks = ["Cartoonize"]
+    feature_choice = st.sidebar.selectbox("Find features", tasks)
     if st.button("Process"):
-        if feature_choice=="Cartoonize":
-            result_img = cartoonize_image(our_image)
+        if feature_choice == "Cartoonize":
+            result_img = cartoonize_image(enhanced_img)
             st.image(result_img)
-        
-        
-        
-        
-        
-elif choice=="About":
-    st.subheader("About the developer")
-    st.markdown("Built with streamlit by [Team1]")
-    st.text("Our team built this as a Mini-Project for Cloud Computing")
-    clou=Image.open("Aboutfor.jpg")
-    st.image(clou)
-    st.text("We have a basic understanding on Python and C languages")
